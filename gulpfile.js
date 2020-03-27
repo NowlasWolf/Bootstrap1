67a5b48328cdfc5f721579d4a60478723a08715d
Alexander <NowlasWolf@users.noreply.github.com>
2020-03-27 03:35:53 -0700

    Upload for new branch

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('serve', gulp.series('sass', function() {
         server: "./src"  
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss', 'sass']).on('change', browserSync.reload);;
+    gulp.watch(['src/scss/*.scss']).on('change', gulp.series('sass',browserSync.reload));
+
     gulp.watch("src/*.html").on('change', browserSync.reload);
 }));
 
